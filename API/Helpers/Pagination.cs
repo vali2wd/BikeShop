abf766880a197ce5d9f9f2318c0be2bf4b6fbe5a
DESKTOP-I7HVH6A\Vali <vali_nafo@yahoo.com>
2022-12-27 20:55:27 +0200

    Pagination, filtering, search.

---
+++ API/Helpers/Pagination.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class Pagination<T> where T : class
+    {
+        public Pagination(int pageIndex, int pageSize, int count, IEnumerable<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Count = count;
+            Data = data;
+        }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+
+        public IEnumerable<T> Data { get; set; }
+    }
+}
